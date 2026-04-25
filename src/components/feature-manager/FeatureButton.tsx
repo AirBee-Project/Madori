d117e73214a56a1e01bcbe6c703383126de5c2fb
Tomoro0726 <s.tomoro0726@gmail.com~>
2026-04-26 08:25:55 +0900

    丁寧なJsDocを追加

--- src/components/feature-manager/FeatureButton.tsx
+++ src/components/feature-manager/FeatureButton.tsx
@@ -1,14 +1,29 @@
 import type { Icon, IconProps } from "@tabler/icons-react";
 
-type ButtonProps = {
+type FeatureButtonProps = {
+  /** Featureの名前
+   * @example "点"
+   */
   name: string;
+
+  /** アイコン */
   icon: React.ForwardRefExoticComponent<IconProps & React.RefAttributes<Icon>>;
+
+  /** 現在アクティブかどうか。
+   * アクティブな場合は色が変更される。
+   */
   isActive: boolean;
+
+  /** クリック時に呼ばれる関数 */
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
-export default function FeatureButton(props: ButtonProps) {
+/**
+ * @description FeatureManagerの個別のボタンを作成するためのコンポーネント。
+ */
+export default function FeatureButton(props: FeatureButtonProps) {
   return (
-    <button type="button">
+    <button type="button" onClick={props.onClick}>
       {props.name}
       <props.icon />
     </button>
--- src/components/feature-manager/FeatureManager.tsx
+++ src/components/feature-manager/FeatureManager.tsx
@@ -1,13 +1,15 @@
 import { IconTableMinus } from "@tabler/icons-react";
 import FeatureButton from "./FeatureButton";
 
-/** 地図上に点、線、空間IDなどを追加するためのUI。
- * 画面左上に表示される。
+/**
+ * @description 地図上に点、線、空間IDなどを追加するためのUI。画面左上に表示される。
  */
 export default function FeatureManager() {
   return (
     <div>
-      neko
+      <FeatureButton name={"点"} icon={IconTableMinus} isActive={false} />
+      <FeatureButton name={"点"} icon={IconTableMinus} isActive={false} />
+      <FeatureButton name={"点"} icon={IconTableMinus} isActive={false} />
       <FeatureButton name={"点"} icon={IconTableMinus} isActive={false} />
     </div>
   );
