cb09f86eda71103c1d7123281fcce2d0abe040f1
o1ghol <suzu@nunu.org>
2026-04-30 11:33:18 +0900

    chore;型ファイルの移動

--- src/types/point.ts
+++ src/types/point.ts
@@ -5,5 +5,7 @@ export const PointSchema = z.object({
     longitude: z.number().min(-180, "経度は-180から180の間で入力してください").max(180, "経度は-180から180の間で入力してください"),
     altitude: z.number().min(0, "高度は0以上で入力してください").default(0)
 });
-
+/**
+ * zodによる点の型定義
+ */
 export type Point = z.infer<typeof PointSchema>;
