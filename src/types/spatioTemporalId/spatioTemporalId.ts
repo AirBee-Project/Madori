6218b8763986a9cad9239ab408738aba82ec8661
Tomoro Saito <s.tomoro0726@gmail.com>
2026-05-11 17:34:56 +0900

    コメントを変更

--- src/types/spatioTemporalId/spatioTemporalId.ts
+++ src/types/spatioTemporalId/spatioTemporalId.ts
@@ -4,6 +4,8 @@ import { TemporalIdSchema } from "./temporalId";
 
 /**
  * 時空間ID
+ *
+ * 時間ID部分がない場合は、永久に存在する空間IDを示す。
  */
 export const SpatioTemporalIdSchema = z.object({
   spatialId: SpatialIdSchema,
