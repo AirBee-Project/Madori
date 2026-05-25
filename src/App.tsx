4274320a8dba61a5799905f2fc327eb99fa05380
Tomoro Saito <s.tomoro0726@gmail.com>
2026-05-26 00:39:22 +0900

    コメントを修正

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ export default function App() {
         <FeatureManager />
       </div>
 
-      {/* drowmodemanager */}
+      {/* drawmodemanager */}
       <div
         style={{
           position: "absolute",
